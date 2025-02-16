832d088e05809a1987e8df3ffd04aba98aec6c5c
Tiago Freire Barbosa <tfreirebar@gmail.com>
2025-02-16 14:54:57 -0300

    Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text('Tarefas')),
         body: ListView(
           children: [
-            Task('Aprender Flutter no café da manhã comendo sucrilhos e assistindo desenho'),
+            Task(
+              'Aprender Flutter no café da manhã comendo sucrilhos e assistindo desenho',
+            ),
             Task('Andar de Bike'),
             Task('Meditar'),
           ],
@@ -30,38 +32,64 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
-
+class Task extends StatefulWidget {
   final String nome;
+
   const Task(this.nome, {super.key});
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+
+  int nivel = 1;
+
+  @override
   Widget build(BuildContext context) {
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
         child: Stack(
           children: [
             Container(color: Colors.blue, height: 140),
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(color: Colors.black26, width: 72, height: 100),
-                  Container(
-                    width: 200,
-                    child: Text(nome,
-                      style: TextStyle(
-                        fontSize: 24,
-                        overflow: TextOverflow.ellipsis
-                      )
-                    ),
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(color: Colors.black26, width: 72, height: 100),
+                      Container(
+                        width: 200,
+                        child: Text(
+                          widget.nome,
+                          style: TextStyle(
+                            fontSize: 24,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            nivel++;
+                          });
+                          print(nivel);
+                          },
+                        child: Icon(Icons.arrow_drop_up),
+                      ),
+                    ],
                   ),
-                  ElevatedButton(onPressed: () {}, child: Icon(Icons.arrow_drop_up)),
-                ],
-              ),
+                ),
+                Text(
+                  'Nível $nivel',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ],
             ),
           ],
         ),
@@ -69,5 +97,3 @@ class Task extends StatelessWidget {
     );
   }
 }
-
-
