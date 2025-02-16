73a17bbfacec71ac412945f81afa5f3351d4b0ad
Tiago Freire Barbosa <tfreirebar@gmail.com>
2025-02-16 13:16:00 -0300

    Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,49 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: ,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Tarefas')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task('Aprender Flutter'),
+            Task('Andar de Bike'),
+            Task('Meditar'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
     );
   }
 }
+
+class Task extends StatelessWidget {
+
+  final String nome;
+  const Task(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(color: Colors.blue, height: 140),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(color: Colors.black26, width: 72, height: 100),
+                Text(nome),
+                ElevatedButton(onPressed: () {}, child: Icon(Icons.arrow_drop_up)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
