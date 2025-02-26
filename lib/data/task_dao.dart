fac65c2f9ab14f79fd84cece33ed87dc42c35a03
Tiago Freire Barbosa <tfreirebar@gmail.com>
2025-02-25 22:28:53 -0300

    DAO

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:firstappflutter/components/task.dart';
+import 'package:firstappflutter/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tableSql =
@@ -12,13 +14,84 @@ class TaskDao {
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
 
-  save(Task task) async {}
+  save(Task task) async {
+    print('Salvando tarefa: $task');
+
+    final Database db = await getDatabase();
+    var itemExists = await find(task.nome);
+    Map<String, dynamic> taskMap = toMap(task);
+
+    if (itemExists.isEmpty) {
+      return await db.insert(_tableName, taskMap);
+    } else {
+      print('A Tarefa já existe');
+      return await db.update(
+          _tableName, taskMap, where: '$_name = ?', whereArgs: [task.nome]);
+    }
+  }
 
   Future<List<Task>> findAll() async {
     print('findAll');
+
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tableName);
+
+    print('Procurando dados: $result');
+
+    return toList(result);
   }
 
-  Future<List<Task>> find(String taskName) async {}
+  List<Task> toList(List<Map<String, dynamic>> taskList) {
+    print('ToList');
+    final List<Task> tasks = [];
 
-  delete(String taskName) async {}
+    for (Map<String, dynamic> line in taskList) {
+      final Task task = Task(
+          line[_name],
+          line[_image],
+          line[_difficulty]);
+
+          tasks.add(task);
+    }
+
+    print('lista de tarefas: $tasks');
+    return tasks;
+  }
+
+  Future<List<Task>> find(String taskName) async {
+    print('Find:');
+
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(
+      _tableName,
+      where: '$_name = ?',
+      whereArgs: [taskName],
+    );
+
+    print('Tarefa encontrada: ${toList(result)}');
+
+    return toList(result);
+  }
+
+  delete(String taskName) async {
+    print('Delete');
+
+    final Database db = await getDatabase();
+    return db.delete(
+      _tableName,
+      where: '$_name = ?',
+      whereArgs: [taskName],
+    );
+  }
+
+  Map<String, dynamic> toMap(Task task){
+
+    print('toMap');
+
+    final Map<String, dynamic> taskMap = Map();
+    taskMap[_name] = task.nome;
+    taskMap[_difficulty] = task.dificuldade;
+    taskMap[_image] = task.foto;
+    return taskMap;
+  }
 }
