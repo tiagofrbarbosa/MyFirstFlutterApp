9cfc27c47b846eca4e44ec683d93217c98552902
Tiago Freire Barbosa <tfreirebar@gmail.com>
2025-03-01 13:54:14 -0300

    Implementação do FutureBuilder para conexão com a camada DAO

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -3,6 +3,8 @@ import 'package:firstappflutter/data/task_inherited.dart';
 import 'package:firstappflutter/screens/form_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../data/task_dao.dart';
+
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
 
@@ -15,15 +17,75 @@ class _InitialScreenState extends State<InitialScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(leading: Container(), title: Text('Tarefas')),
-      body: ListView(
-        children: TaskInherited.of(context).taskList,
+      body: Padding(
         padding: EdgeInsets.only(top: 8, bottom: 70),
+        child: FutureBuilder<List<Task>>(
+          future: TaskDao().findAll(),
+          builder: (context, snapshot) {
+            List<Task>? items = snapshot.data;
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              return Center(
+                child: Column(
+                  children:[
+                      CircularProgressIndicator(),
+                    Text('Carregando...')
+                  ],
+                ),
+              );
+                throw UnimplementedError();
+              case ConnectionState.waiting:
+                return Center(
+                  child: Column(
+                    children:[
+                      CircularProgressIndicator(),
+                      Text('Carregando...')
+                    ],
+                  ),
+                );
+                throw UnimplementedError();
+              case ConnectionState.active:
+                return Center(
+                  child: Column(
+                    children:[
+                      CircularProgressIndicator(),
+                      Text('Carregando...')
+                    ],
+                  ),
+                );
+                throw UnimplementedError();
+              case ConnectionState.done:
+                if (snapshot.hasData && items != null) {
+                  if (items.isNotEmpty) {
+                    return ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final Task task = items[index];
+                        return task;
+                      },
+                    );
+                  }
+                  return Center(
+                    child: Column(
+                    children: [
+                      Icon(Icons.error_outline, size: 128),
+                      Text('Nenhuma tarefa cadastrada!',
+                          style: TextStyle(fontSize: 32)),],
+                     ),
+                  );
+                }
+                throw UnimplementedError();
+            }
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (contextNew) => FormScreen(taskContext: context,)),
+            MaterialPageRoute(
+              builder: (contextNew) => FormScreen(taskContext: context),
+            ),
           );
         },
         child: Icon(Icons.add),
